8717c46687bd29c5633631377475edb03f2f9a3f
Karl Beksa <karlbeksa@gmail.com>
2019-05-07 15:39:39 -0400

    mvp complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,11 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const firstName = "Karl";
+function writeName(name){
+  const lastName=name;
+  console.log(`My name is ${firstName} ${lastName}`);
+}
+writeName("Beksa");
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
